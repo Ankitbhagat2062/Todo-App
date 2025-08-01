fef637e164f4274b66afc3ce2a5bbedd6ee9dc8b
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-08-01 09:10:58 +0545

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; //If you are cloning then No need of this line
 
-dotenv.config();
+dotenv.config(); //If you are cloning then No need of this line
 
 const conn = async () => {
     try {
-        const mongoUri = process.env.MONGO_URI;
-        if (!mongoUri) {
+        const mongoUri = process.env.MONGO_URI;//If you are cloning my repo then replace process.env.Mongo_URI with you url MongoDb cluster url.
+        if (!mongoUri) {// sample url mongodb+srv://<username>:<password><Clustername>.ks8o7ls.mongodb.net/
             throw new Error('MONGO_URI environment variable is not set');
         }
         await mongoose.connect(mongoUri);
