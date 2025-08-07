dc308761a18c8f3555708fc0d0ad920af5c94d76
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-08-07 18:29:50 +0545

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique:true, required: true },
   username: { type: String, unique:true, required: true},
   password: { type: String, required: true },
-  list:[{type:mongoose.Types.ObjectId , ref:"List"},]
+  list:[{type:mongoose.Types.ObjectId , ref:"List"},],
+  Event:[{type:mongoose.Types.ObjectId , ref:"Event"},]
 });
 
 export default mongoose.model('User', userSchema);
